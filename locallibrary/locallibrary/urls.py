ca18d5bbf9ee9f39b2aa6302022ee084c3b3931a
Fenrir <fenrireclair@gmail.com>
2022-11-20 17:49:30 +0900

    Add URL maps to redirect the base URL to our application

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+# Add URL maps to redirect the base URL to our application
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
