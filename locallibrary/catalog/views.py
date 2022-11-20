f562bce8cfb06822fde0e6ebe406fe44b7f1885d
Fenrir <fenrireclair@gmail.com>
2022-11-20 23:56:50 +0900

    編寫 index 方法，返回index.html

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from .models import Book, Author, BookInstance, Genre
 # Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a') 可借閱的
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+    # Render the HTML template index.html with the data in the context variable
+    # 調用 render() 函數來創建並返回 HTML 頁面作為響應
+    # context 變量包含將插入到這些佔位符中的數據的 Python 字典）為參數
+    return render(request, 'index.html', context=context)
