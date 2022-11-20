49c56d17f290b87b2b614bece86718378f91be88
Fenrir <fenrireclair@gmail.com>
2022-11-20 22:00:06 +0900

    註冊一個 模型管理 類別 (ModelAdmin class)

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Genre)
-admin.site.register(BookInstance)
+# admin.site.register(Book)
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+# admin.site.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+
+# admin.site.register(BookInstance)
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Language)
+admin.site.register(Genre)
