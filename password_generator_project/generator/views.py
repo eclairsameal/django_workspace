5447fc36420a139b9757942c26c4ca7ce3fc2185
Fenrir <fenrireclair@gmail.com>
2022-12-01 16:41:03 +0900

    亂數產生長度為10的密碼

--- password_generator_project/generator/views.py
+++ password_generator_project/generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import  random
 
 # Create your views here.
 
@@ -12,4 +13,12 @@ def eggs(request):
     return HttpResponse("<h1>Eggs are so tasty</h1>")
 
 def password(request):
-    return render(request, 'generator/password.html')
+    char_list = list('abcdefghijklmnopqrstuvwxyz')
+    length = 10
+    thepassword = ""
+    
+    for char in range(length):
+        thepassword += random.choice(char_list)
+    
+    return render(request, 'generator/password.html', {"password":thepassword})
+
