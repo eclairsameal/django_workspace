92cfe30d09963630b37a0ef44b10acaa6651981a
Fenrir <fenrireclair@gmail.com>
2022-12-06 05:42:32 +0900

    照日期編排取前5個

--- personal_portfolio_project/blog/views.py
+++ personal_portfolio_project/blog/views.py
@@ -3,5 +3,6 @@ from .models import Blog
 
 # Create your views here.
 def all_blogs(request):
-    blogs = Blog.objects.all()
+    # blogs = Blog.objects.all()
+    blogs = Blog.objects.order_by('-date')[:5]   # 照日期編排取前5個
     return render(request, "blog/all_blogs.html", {"blogs":blogs})
