a33a6bfdb135f10093c51074eab11c6fc9630e6e
Fenrir <fenrireclair@gmail.com>
2022-12-06 07:07:33 +0900

    利用 url 傳值，並顯示到html上

--- personal_portfolio_project/blog/urls.py
+++ personal_portfolio_project/blog/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path("", views.all_blogs, name="all_blogs"),
+    path('<int:blog_id>/', views.detail, name='detail'),  # 會傳數字給views.detail
 ]
+
+"""
+    path("1", views.detail, name="detail"),
+    path("2", views.detail, name="detail"),
+    path("3", views.detail, name="detail"),
+    * 注意 <int:blog_id> 分號兩邊不能有空白
+"""
--- personal_portfolio_project/blog/views.py
+++ personal_portfolio_project/blog/views.py
@@ -6,3 +6,6 @@ def all_blogs(request):
     # blogs = Blog.objects.all()
     blogs = Blog.objects.order_by('-date')[:5]   # 照日期編排取前5個
     return render(request, "blog/all_blogs.html", {"blogs":blogs})
+
+def detail(request, blog_id):
+    return render(request, "blog/detail.html", {"id": blog_id})
