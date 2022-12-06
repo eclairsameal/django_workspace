2ee832095139cf4b24cdbb9c2609f0c108cb41b1
Fenrir <fenrireclair@gmail.com>
2022-12-06 19:41:04 +0900

    載入url更安全的寫法

--- personal_portfolio_project/blog/urls.py
+++ personal_portfolio_project/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'blog'    # 指定應用程式名稱
+
 urlpatterns = [
     path("", views.all_blogs, name="all_blogs"),
     path('<int:blog_id>/', views.detail, name='detail'),  # 會傳數字給views.detail
