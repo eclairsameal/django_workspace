2c6c550554ab00a2b7285b984410f867b26fb80d
Fenrir <fenrireclair@gmail.com>
2022-12-06 19:10:06 +0900

    處理找不到頁面

--- personal_portfolio_project/blog/views.py
+++ personal_portfolio_project/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
@@ -8,4 +8,6 @@ def all_blogs(request):
     return render(request, "blog/all_blogs.html", {"blogs":blogs})
 
 def detail(request, blog_id):
-    return render(request, "blog/detail.html", {"id": blog_id})
+    blog = get_object_or_404(Blog, pk=blog_id) # pk 是主鍵的縮寫
+    # return render(request, "blog/detail.html", {"id": blog_id})
+    return render(request, "blog/detail.html", {"blog": blog})
