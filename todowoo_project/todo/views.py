52809b86c1b46baacbe02d53cabcf83df88fbda1
Fenrir <fenrireclair@gmail.com>
2022-12-12 20:21:43 +0900

    修改錯字

--- todowoo_project/todo/views.py
+++ todowoo_project/todo/views.py
@@ -7,8 +7,9 @@ def sigunpuser(request):
         return render(request, 'todo/sigunpuser.html', {'form':UserCreationForm()})
     else:
         # Create a new user
-        if request.POST['password1'] == request.POST['password']:    # 檢查密碼
+        if request.POST['password1'] == request.POST['password2']:    # 檢查密碼
             user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
             user.save()    # 上面只是設定值，設定完後要用save存到資料庫裡
         else:
+            pass
             # Tell the user the passwords didn't match
