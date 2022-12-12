b6ca669ef5a52026c5bdc3ed6887497a4932521c
Fenrir <fenrireclair@gmail.com>
2022-12-12 19:59:25 +0900

    create user to database

--- todowoo_project/todo/views.py
+++ todowoo_project/todo/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import User
 # Create your views here.
 def sigunpuser(request):
-    return render(request, 'todo/sigunpuser.html', {'form':UserCreationForm()})
+    if request.method == 'GET':    # 檢查是否為GET方法
+        return render(request, 'todo/sigunpuser.html', {'form':UserCreationForm()})
+    else:
+        # Create a new user
+        if request.POST['password1'] == request.POST['password']:    # 檢查密碼
+            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+            user.save()    # 上面只是設定值，設定完後要用save存到資料庫裡
+        else:
+            # Tell the user the passwords didn't match
