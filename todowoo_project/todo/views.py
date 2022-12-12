d43802a699c6d872f50b7e7575c25a6cd6b7b99a
Fenrir <fenrireclair@gmail.com>
2022-12-12 20:39:38 +0900

    重複的用戶名錯誤跟密碼確認錯誤

--- todowoo_project/todo/views.py
+++ todowoo_project/todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 # Create your views here.
 def sigunpuser(request):
     if request.method == 'GET':    # 檢查是否為GET方法
@@ -8,8 +9,15 @@ def sigunpuser(request):
     else:
         # Create a new user
         if request.POST['password1'] == request.POST['password2']:    # 檢查密碼
-            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-            user.save()    # 上面只是設定值，設定完後要用save存到資料庫裡
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()    # 上面只是設定值，設定完後要用save存到資料庫裡
+            except IntegrityError:
+                return render(request, 'todo/sigunpuser.html', {'form':UserCreationForm(),
+                          'error':'That username has already been taken. Please choose a new username.'})
+                
         else:
-            pass
             # Tell the user the passwords didn't match
+            return render(request, 'todo/sigunpuser.html', {'form':UserCreationForm(),
+                          'error':'Passwords did not match'})
+            
