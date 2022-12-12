f2c57e2519c41485bdad24c89c514173156813aa
Fenrir <fenrireclair@gmail.com>
2022-12-12 16:47:57 +0900

    利用預設FORM產生Sign Up表單

--- todowoo_project/todo/views.py
+++ todowoo_project/todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def sigunpuser(request):
-    return render(request, 'todo/sigunpuser.html')
+    return render(request, 'todo/sigunpuser.html', {'form':UserCreationForm()})
